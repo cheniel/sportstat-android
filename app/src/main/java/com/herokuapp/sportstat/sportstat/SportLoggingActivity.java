c5d7b3781072e00cd961d529e791b420662495af
cheniel <danielchen627@gmail.com>
2015-02-26 01:15:40 -0500

    SportLoggingActivity now detects if Pebble is connected

--- app/src/main/java/com/herokuapp/sportstat/sportstat/SportLoggingActivity.java
+++ app/src/main/java/com/herokuapp/sportstat/sportstat/SportLoggingActivity.java
@@ -1,18 +1,30 @@
 package com.herokuapp.sportstat.sportstat;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.getpebble.android.kit.PebbleKit;
+
+import java.util.UUID;
 
 
 public class SportLoggingActivity extends Activity {
+    private final static UUID PEBBLE_APP_UUID = UUID.fromString("fe649006-bd16-4f05-a0c3-060750535107");
     private GameBasketball mGame;
     private TextView mAssistsView;
     private TextView mTwoPointsView;
     private TextView mThreePointsView;
+    private BroadcastReceiver mPebbleConnectedReceiver;
+    private BroadcastReceiver mPebbleDisconnectedReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +34,46 @@ public class SportLoggingActivity extends Activity {
         mAssistsView = (TextView) findViewById(R.id.new_basketball_game_assist_text_view);
         mTwoPointsView = (TextView) findViewById(R.id.new_basketball_game_two_point_text_view);
         mThreePointsView = (TextView) findViewById(R.id.new_basketball_game_three_point_text_view);
-
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // detect if watch is connected
+        boolean connected = PebbleKit.isWatchConnected(getApplicationContext());
+        Log.i(getLocalClassName(), "Pebble is " + (connected ? "connected" : "not connected"));
+        if (connected) {
+            Toast.makeText(this, "Pebble is connected!", Toast.LENGTH_SHORT).show();
+        }
+
+        // open up SportStat Pebble app
+        PebbleKit.startAppOnPebble(getApplicationContext(), PEBBLE_APP_UUID);
+
+        // register receivers for Pebble disconnect and connect
+        mPebbleConnectedReceiver = PebbleKit.registerPebbleConnectedReceiver(this, new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.i(getLocalClassName(), "Pebble connected!");
+                Toast.makeText(getApplicationContext(), "Connected!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mPebbleDisconnectedReceiver = PebbleKit.registerPebbleDisconnectedReceiver(this, new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.i(getLocalClassName(), "Pebble disconnected!");
+                Toast.makeText(getApplicationContext(), "Pebble disconnected!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mPebbleConnectedReceiver);
+        unregisterReceiver(mPebbleDisconnectedReceiver);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
