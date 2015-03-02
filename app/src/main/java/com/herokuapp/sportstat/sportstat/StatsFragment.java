66e5c22547936f62ef5adfe1c6e7bd884767bcc8
jrigby99 <john.n.rigby.17@dartmouth.edu>
2015-03-02 04:43:56 -0500

    Making the Chart in FriendViewActivity better. Modified BasketBall game to have a public method returning just the Month Day Year of a basketball game

--- app/src/main/java/com/herokuapp/sportstat/sportstat/StatsFragment.java
+++ app/src/main/java/com/herokuapp/sportstat/sportstat/StatsFragment.java
@@ -9,11 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import org.achartengine.ChartFactory;
 import org.achartengine.chart.BarChart.Type;
+import org.achartengine.chart.PointStyle;
 import org.achartengine.model.XYMultipleSeriesDataset;
 import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.BasicStroke;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 
@@ -35,23 +38,20 @@ import android.widget.LinearLayout;
  * create an instance of this fragment.
  */
 public class StatsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+
+
 
     private View mChart;
-    private String[] mMonth = new String[] {
-            "Jan", "Feb" , "Mar", "Apr", "May", "Jun",
-            "Jul", "Aug" , "Sep", "Oct", "Nov", "Dec"
-    };
+//    private String[] mMonth = new String[] {
+//            "Jan", "Feb" , "Mar", "Apr", "May", "Jun",
+//            "Jul", "Aug" , "Sep", "Oct", "Nov", "Dec"
+//    };
 
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     private OnFragmentInteractionListener mListener;
+    private ArrayList<BasketballGame> mGamesArray;
 
     /**
      * Use this factory method to create a new instance of
@@ -65,8 +65,7 @@ public class StatsFragment extends Fragment {
     public static StatsFragment newInstance(String param1, String param2) {
         StatsFragment fragment = new StatsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -78,150 +77,195 @@ public class StatsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
 
     }
 
 
     private void openChart(){
-        int[] x = { 0,1,2,3,4,5,6,7, 8, 9, 10, 11 };
-        int[] income = { 2000,2500,2700,3000,2800,3500,3700,3800, 0,0,0,0};
-        int[] expense = {2200, 2700, 2900, 2800, 2600, 3000, 3300, 3400, 0, 0, 0, 0 };
 
-        // Creating an XYSeries for Income
-        XYSeries incomeSeries = new XYSeries("Income");
-        // Creating an XYSeries for Expense
-        XYSeries expenseSeries = new XYSeries("Expense");
-        // Adding data to Income and Expense Series
-        for(int i=0;i<x.length;i++){
-            incomeSeries.add(i,income[i]);
-            expenseSeries.add(i,expense[i]);
+        int[] x = new int[mGamesArray.size()];
+        int[] assists = new int[mGamesArray.size()];
+        int[] twos = new int[mGamesArray.size()];
+        int[] threes = new int[mGamesArray.size()];
+
+
+        for(int j = 0; j < mGamesArray.size(); j++){
+            x[j] = j;
+            assists[j] = mGamesArray.get(j).getAssists();
+            twos[j] = mGamesArray.get(j).getTwoPoints();
+            threes[j] = mGamesArray.get(j).getThreePoints();
         }
 
-        // Creating a dataset to hold each series
-        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
-        // Adding Income Series to the dataset
-        dataset.addSeries(incomeSeries);
-        // Adding Expense Series to dataset
-        dataset.addSeries(expenseSeries);
 
-        // Creating XYSeriesRenderer to customize incomeSeries
+
+// Creating an XYSeries for Income
+        XYSeries assistSeries = new XYSeries("Assists");
+// Creating an XYSeries for Expense
+        XYSeries twoSeries = new XYSeries("2-Points");
+
+        XYSeries threeSeries = new XYSeries("3-Points");
+
+        for(int i=0;i<x.length;i++){
+            assistSeries.add(i,assists[i]);
+            twoSeries.add(i,twos[i]);
+            threeSeries.add(i, threes[i]);
+        }
+
+// Creating a dataset to hold each series
+        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+// Adding Income Series to the dataset
+        dataset.addSeries(assistSeries);
+// Adding Expense Series to dataset
+        dataset.addSeries(twoSeries);
+
+        dataset.addSeries(threeSeries);
+
+// Creating XYSeriesRenderer to customize incomeSeries
         XYSeriesRenderer incomeRenderer = new XYSeriesRenderer();
         incomeRenderer.setColor(Color.CYAN); //color of the graph set to cyan
         incomeRenderer.setFillPoints(true);
-        incomeRenderer.setLineWidth(2);
+        incomeRenderer.setLineWidth(4f);
         incomeRenderer.setDisplayChartValues(true);
-        incomeRenderer.setDisplayChartValuesDistance(10); //setting chart value distance
+//setting chart value distance
+        incomeRenderer.setDisplayChartValuesDistance(10);
+//setting line graph point style to circle
+        incomeRenderer.setPointStyle(PointStyle.CIRCLE);
+//setting stroke of the line chart to solid
+        incomeRenderer.setStroke(BasicStroke.SOLID);
 
-        // Creating XYSeriesRenderer to customize expenseSeries
+// Creating XYSeriesRenderer to customize expenseSeries
         XYSeriesRenderer expenseRenderer = new XYSeriesRenderer();
         expenseRenderer.setColor(Color.GREEN);
         expenseRenderer.setFillPoints(true);
-        expenseRenderer.setLineWidth(2);
+        expenseRenderer.setLineWidth(4f);
         expenseRenderer.setDisplayChartValues(true);
+//setting line graph point style to circle
+        expenseRenderer.setPointStyle(PointStyle.SQUARE);
+//setting stroke of the line chart to solid
+        expenseRenderer.setStroke(BasicStroke.SOLID);
 
-        // Creating a XYMultipleSeriesRenderer to customize the whole chart
+
+        // Creating XYSeriesRenderer to customize expenseSeries
+        XYSeriesRenderer threeRenderer = new XYSeriesRenderer();
+        expenseRenderer.setColor(Color.RED);
+        expenseRenderer.setFillPoints(true);
+        expenseRenderer.setLineWidth(4f);
+        expenseRenderer.setDisplayChartValues(true);
+//setting line graph point style to circle
+        expenseRenderer.setPointStyle(PointStyle.SQUARE);
+//setting stroke of the line chart to solid
+        expenseRenderer.setStroke(BasicStroke.SOLID);
+
+// Creating a XYMultipleSeriesRenderer to customize the whole chart
         XYMultipleSeriesRenderer multiRenderer = new XYMultipleSeriesRenderer();
-        multiRenderer.setOrientation(XYMultipleSeriesRenderer.Orientation.HORIZONTAL);
         multiRenderer.setXLabels(0);
-        multiRenderer.setChartTitle("Income vs Expense Chart");
-        multiRenderer.setXTitle("Year 2014");
-        multiRenderer.setYTitle("Amount in Dollars");
+        multiRenderer.setChartTitle(getString(R.string.stats_chart_title));
 
-        /***
-         * Customizing graphs
-         */
+        multiRenderer.setXTitle("Entries from "+mGamesArray.get(0).getTimeString()
+                +" to "+mGamesArray.get(mGamesArray.size()-1).getTimeString());
+        //multiRenderer.setXTitle(getString(R.string.stats_chart_x_label));
+       // multiRenderer.setYTitle(getString(R.string.stats_chart_y_label));
+
+
+/***
+ * Customizing graphs
+ */
 //setting text size of the title
         multiRenderer.setChartTitleTextSize(28);
-        //setting text size of the axis title
+//setting text size of the axis title
         multiRenderer.setAxisTitleTextSize(24);
-        //setting text size of the graph lable
+//setting text size of the graph lable
         multiRenderer.setLabelsTextSize(24);
-        //setting zoom buttons visiblity
+
+        multiRenderer.setLegendTextSize(30);
+//setting zoom buttons visiblity
         multiRenderer.setZoomButtonsVisible(false);
-        //setting pan enablity which uses graph to move on both axis
+//setting pan enablity which uses graph to move on both axis
         multiRenderer.setPanEnabled(false, false);
-        //setting click false on graph
+//setting click false on graph
         multiRenderer.setClickEnabled(false);
-        //setting zoom to false on both axis
+//setting zoom to false on both axis
         multiRenderer.setZoomEnabled(false, false);
-        //setting lines to display on y axis
-        multiRenderer.setShowGridY(false);
-        //setting lines to display on x axis
-        multiRenderer.setShowGridX(false);
-        //setting legend to fit the screen size
+//setting lines to display on y axis
+        multiRenderer.setShowGridY(true);
+//setting lines to display on x axis
+        multiRenderer.setShowGridX(true);
+//setting legend to fit the screen size
         multiRenderer.setFitLegend(true);
-        //setting displaying line on grid
-        multiRenderer.setShowGrid(false);
-        //setting zoom to false
+//setting displaying line on grid
+        multiRenderer.setShowGrid(true);
+//setting zoom to false
         multiRenderer.setZoomEnabled(false);
-        //setting external zoom functions to false
+//setting external zoom functions to false
         multiRenderer.setExternalZoomEnabled(false);
-        //setting displaying lines on graph to be formatted(like using graphics)
+//setting displaying lines on graph to be formatted(like using graphics)
         multiRenderer.setAntialiasing(true);
-        //setting to in scroll to false
+//setting to in scroll to false
         multiRenderer.setInScroll(false);
-        //setting to set legend height of the graph
+//setting to set legend height of the graph
         multiRenderer.setLegendHeight(30);
-        //setting x axis label align
+//setting x axis label align
         multiRenderer.setXLabelsAlign(Align.CENTER);
-        //setting y axis label to align
+//setting y axis label to align
         multiRenderer.setYLabelsAlign(Align.LEFT);
-        //setting text style
+//setting text style
         multiRenderer.setTextTypeface("sans_serif", Typeface.NORMAL);
-        //setting no of values to display in y axis
+//setting no of values to display in y axis
         multiRenderer.setYLabels(10);
-        // setting y axis max value, Since i'm using static values inside the graph so i'm setting y max value to 4000.
-        // if you use dynamic values then get the max y value and set here
-        multiRenderer.setYAxisMax(4000);
-        //setting used to move the graph on xaxiz to .5 to the right
+// setting y axis max value, Since i'm using static values inside the graph so i'm setting y max value to 4000.
+// if you use dynamic values then get the max y value and set here
+        multiRenderer.setYAxisMax(Math.max(Math.max(findMax(assists), findMax(twos)), findMax(threes)));
+//setting used to move the graph on xaxiz to .5 to the right
         multiRenderer.setXAxisMin(-0.5);
-//setting max values to be display in x axis
+//setting used to move the graph on xaxiz to .5 to the right
         multiRenderer.setXAxisMax(11);
-        //setting bar size or space between two bars
-        multiRenderer.setBarSpacing(0.5);
-        //Setting background color of the graph to transparent
+//setting bar size or space between two bars
+//multiRenderer.setBarSpacing(0.5);
+//Setting background color of the graph to transparent
         multiRenderer.setBackgroundColor(Color.TRANSPARENT);
-        //Setting margin color of the graph to transparent
+//Setting margin color of the graph to transparent
         multiRenderer.setMarginsColor(getResources().getColor(R.color.transparent_background));
         multiRenderer.setApplyBackgroundColor(true);
+        multiRenderer.setScale(2f);
+//setting x axis point size
+        multiRenderer.setPointSize(4f);
+//setting the margin size for the graph in the order top, left, bottom, right
+        multiRenderer.setMargins(new int[]{70, 30, 70, 70});
 
-        //setting the margin size for the graph in the order top, left, bottom, right
-        multiRenderer.setMargins(new int[]{30, 30, 30, 30});
+//        for(int i=0; i< x.length;i++){
+//            multiRenderer.addXTextLabel(i, mMonth[i]);
+//        }
 
-        for(int i=0; i< x.length;i++){
-            multiRenderer.addXTextLabel(i, mMonth[i]);
-        }
-
-        // Adding incomeRenderer and expenseRenderer to multipleRenderer
-        // Note: The order of adding dataseries to dataset and renderers to multipleRenderer
-        // should be same
+// Adding incomeRenderer and expenseRenderer to multipleRenderer
+// Note: The order of adding dataseries to dataset and renderers to multipleRenderer
+// should be same
         multiRenderer.addSeriesRenderer(incomeRenderer);
         multiRenderer.addSeriesRenderer(expenseRenderer);
+        multiRenderer.addSeriesRenderer(threeRenderer);
 
-        //this part is used to display graph on the xml
+//this part is used to display graph on the xml
         LinearLayout chartContainer = (LinearLayout) getView().findViewById(R.id.profile_stats_chart);
-        //remove any views before u paint the chart
+//remove any views before u paint the chart
         chartContainer.removeAllViews();
-        //drawing bar chart
-        mChart = ChartFactory.getBarChartView(this.getActivity(), dataset, multiRenderer,Type.DEFAULT);
-        //adding the view to the linearlayout
+//drawing bar chart
+        mChart = ChartFactory.getLineChartView(getActivity(), dataset, multiRenderer);
+//adding the view to the linearlayout
         chartContainer.addView(mChart);
 
     }
 
+    //Helper function to find the largest int in an intArray
+    private int findMax(int[] ar) {
+        int max = ar[0];
+        for(int i = 0; i < mGamesArray.size(); i++){
 
-
-
-
-
-
-
-
-
+            if(ar[i] > max){
+                max = ar[i];
+            }
+        }
+        return max;
+    }
 
 
     @Override
@@ -274,12 +318,12 @@ public class StatsFragment extends Fragment {
     public void onResume() {
         super.onResume();
 
-        openChart();
+        //openChart();
     }
 
     //Update graphs in the StatsFragent
     public void updateStats(ArrayList<BasketballGame> gamesArray){
-
+        mGamesArray = gamesArray;
 
         openChart();
 
